88c5ca56bc05a60cb590cc7b601497e0b111b675
Christina Lan <christinalan@Christinas-MBP.home>
2020-10-27 23:00:07 -0400

    getting rid of comment out

--- public/app.js
+++ public/app.js
@@ -1,6 +1,5 @@
-let trashClicked;
-let string;
-let element;
+let today;
+let newDate;
 
 window.addEventListener('load', () => {
     //open and connect socket
@@ -50,9 +49,12 @@ window.addEventListener('load', () => {
     socket.on('msg', (data) => {
         console.log("message arrived!");
         console.log(data);
+    
+        today = new Date();
+        newDate = today.toDateString();
 
     //create a message string and the page element where messages will show up
-    let receivedMsg = data.memory;
+    let receivedMsg = newDate + ' ' + data.memory;
     let msgEl = document.createElement('p');
     msgEl.innerHTML = receivedMsg;
 
@@ -69,7 +71,10 @@ window.addEventListener('load', () => {
         console.log("bad message arrived!");
         console.log(data);
 
-        let receivedMsgBad = data.memory;
+        today = new Date();
+        newDate = today.toDateString();
+
+        let receivedMsgBad = newDate + ' ' + data.memory;
         let badmsgEl = document.createElement('p');
         badmsgEl.innerHTML = receivedMsgBad;
 
@@ -81,87 +86,3 @@ window.addEventListener('load', () => {
 
 });
 
-/*
-    // fetch info from good memory database 
-    document.getElementById('get-happy-tracker').addEventListener('click', () => {
-        fetch('/getGoodMemories')
-        .then(resp => resp.json())
-        .then(data => {
-            document.getElementById('happy-info').innerHTML = '';
-            console.log(data.data);
-            for (let i=0; i< data.data.length; i++) {
-                let string = data.data[i].date + " : " + data.data[i].memory;
-                let element = document.createElement('p');
-                element.innerHTML = string;
-                document.getElementById('happy-info').appendChild(element);
-            }
-        })
-    })
-
-    document.getElementById('button-bad').addEventListener('click', () => {
-        let badMemory = document.getElementById('bad-memories').value;
-        console.log(badMemory);
-
-        let obj2 = {"memory" : badMemory};
-        //stringify the data. server will parse and convert the string back into json object
-        let jsonData = JSON.stringify(obj2);
-
-        //make a fetch request of type POST so that we can send input to the server
-        fetch('/badMemories', {
-            method: 'POST',
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: jsonData
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data); //shows that the server has successfully inserted new memory
-        }) 
-
-        //fetch request to send input to the trash button in the server
-        fetch('/trashBadMem', {
-            method: 'POST',
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: jsonData
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-    })
-
-    document.getElementById('get-bad-tracker').addEventListener('click', () => {
-        fetch('/getBadMemories')
-        .then(resp => resp.json())
-        .then(data => {
-            document.getElementById('bad-info').innerHTML = '';
-            console.log(data.data);
-       
-                for (let i=0; i< data.data.length; i++) {
-                    string = data.data[i].date + " : " + data.data[i].memory;
-                    element = document.createElement('p');
-                    element.innerHTML = string;
-                    document.getElementById('bad-info').appendChild(element);
-                }
-       
-        })
-    })
-
-    document.getElementById('trash-image').addEventListener('click', () => {
-        fetch('/trashedBadMem')
-        .then(resp => resp.json())
-        .then(data => {
-            console.log(data.data);
-        })
-              //if it's clicked, then content disappears
-        document.getElementById('bad-info').innerHTML = '';
-    //   trashClicked = !trashClicked;
-    //   console.log(trashClicked);
-    })
-    
-})
-
-*/
